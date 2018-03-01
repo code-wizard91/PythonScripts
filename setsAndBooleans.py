9aba811bfe61138e6e770f4299edb60dc204158f
Mahboob Ali <alimizan@hotmail.com>
2018-03-01 16:29:28 +0000

    Add files via upload

---
+++ setsAndBooleans.py
@@ -0,0 +1,13 @@
+ x = set() #this is an empty set
+
+x.add(1) #sets contain only keys and only unique elements. this is how you add to a set.
+x.add(2)
+
+l=[1,1,1,1,2,2,2,2,2,2,3,3,3,4,4,4]
+
+print(x)
+print(l)
+
+a = true #this sets the boolean value of 'a' as true
+
+
---
+++ tuples.py
@@ -0,0 +1,9 @@
+#Experimenting with Tuples
+
+#Tuples are immutable and cannot be changed, so they are used to present things that shouldn't be changed
+
+#can also contain different types of elements e.g strings,integers,booleans
+
+tTuple = (1,'mizan',3) #this is how we create a tuple
+
+print(tTuple[2])
