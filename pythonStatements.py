cc23166c4e796571bfb8e32c286a0d3a9e8d5265
Mahboob Ali <alimizan@hotmail.com>
2018-03-03 17:44:04 +0000

    update

---
+++ pythonStatements.py
@@ -0,0 +1,16 @@
+__author__ = 'Mizan'
+
+#Will be exploring differences in statements in python compared to other languages
+
+# if statements in most other languages would look like this
+# ** if(a>b){
+#   a=2;
+#   b=4;
+# }
+
+a = 1
+b = 3
+
+if a<b:             #in python we do not use semi colons to denote the end of a statement, the end of a line is the end of the statement
+    a = 5;
+    print(a)
