c2c78e802cc2d07155303a073ee1d2732b7b0807
Mahboob Ali <alimizan@hotmail.com>
2018-03-07 00:02:59 +0000

    update

---
+++ whileStatement.py
@@ -0,0 +1,15 @@
+__author__ = 'Mizan'
+
+#format for while loop is
+# while test:
+#   //code statement
+# else:
+#   //final statement
+
+x = 0;
+
+while x!=10:
+    x+=1
+    print(x)
+else:
+    print('failed')
