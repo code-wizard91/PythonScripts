9fbed4c3f3ed57db69671f66bfdc43c2b98b34a2
Mahboob Ali <alimizan@hotmail.com>
2018-03-07 00:08:32 +0000

    update

--- whileStatement.py
+++ whileStatement.py
@@ -10,6 +10,14 @@ x = 0;
 
 while x!=10:
     x+=1
-    print(x)
+    print(x)        #this while loop will keep going until x is equal to ten or the test stetement becomes true
 else:
     print('failed')
+
+
+
+y = 0
+
+while y<10:
+    print('y is currently',y)
+    y += 1
