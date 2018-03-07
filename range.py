6a26bb5802f2e366a439590db95f1cf3444a4fbf
Mahboob Ali <alimizan@hotmail.com>
2018-03-07 00:28:50 +0000

    update

---
+++ range.py
@@ -0,0 +1,16 @@
+__author__ = 'Mizan'
+
+#the range function allows you to create a list of numbers ranging from a starting point upto an ending point
+#you can also specify step size
+
+
+
+for z in range(100):        #this for loop prints all numbers upto 100
+    print(z)
+
+
+
+mylist = [1,2,3,534,6547,5678768,789087,4,234,5646,546754756]
+
+for i in range(len(mylist)):
+    print(mylist[i])
